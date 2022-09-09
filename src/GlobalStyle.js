e725da2267d75e5e19a58f6a7797808752e274b6
Tiffany <yang.ding@hotmail.com>
2022-09-08 23:51:23 -0700

    added global style component to index

---
+++ src/GlobalStyle.js
@@ -0,0 +1,41 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  :root {
+    /* COLORS */
+    --primaryBlue: hsla(235, 86, 62, 1);
+    --textColor: hsla(264, 7, 29, 1);
+    --secondaryTextColor: hsla(264, 7, 29, 0.6);
+    --linkColor: hsla(235, 60, 54, 1);
+
+    /* TYPOGRAPHY */
+    --labelFontSize: 1.4rem;
+    --labelLine: 2rem;
+    --labelSpacing: 0.1rem;
+  }
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  html {
+    font-size: 62.5%;
+  }
+  body {
+    font-family: 'Noto Sans', sans-serif;
+  }
+  h1 {
+    font-size: 2.2rem;
+    line-height: 2.8rem;
+  }
+  h2 {
+    font-size: 1.6rem;
+    line-height: 2.4rem;
+    letter-spacing: 0.15rem;
+  }
+  h3 {
+    font-size: 1.4rem;
+    line-height: 2rem;
+    letter-spacing: 0.1rem;
+  }
+`;

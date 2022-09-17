c79d62594f8ff6b7a2eb608e49db9866e18971c2
Tiffany <yang.ding@hotmail.com>
2022-09-16 19:00:07 -0700

    added useForm hook and handleSubmleSubmit function

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -1,13 +1,46 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyledForm } from './style';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import { useForm } from '../../hooks/useForm';
 
 const Form = ({ inputs, btnText, action }) => {
+  let navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post(`http://localhost:8000/${action}`, values)
+      .then((response) => {
+        console.log(response.data);
+      });
+    navigate('/profile');
+  };
+
+  // dynamically initialize values with input fields name
+  const [initialVals, setInitialVals] = useState({});
+  useEffect(() => {
+    let tempObj = {};
+    inputs.forEach((input) => {
+      tempObj[input] = '';
+    });
+    setInitialVals(tempObj);
+  }, []);
+
+  const [values, handleChange] = useForm(initialVals);
+
   return (
-    <StyledForm action={`http://localhost:8000/${action}`} method='post'>
+    <StyledForm onSubmit={handleSubmit}>
       {inputs.map((input) => (
         <div className='input-group' key={input}>
           <label htmlFor={input}>{input}</label>
-          <input type={input} name={input} id={input} required />
+          <input
+            type={input}
+            name={input}
+            id={input}
+            required
+            onChange={handleChange}
+          />
         </div>
       ))}
       <button>
